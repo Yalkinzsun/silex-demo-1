09ed91f9369b9f85fc87af9b2df62094b43c8225
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-05-09 19:04:25 +0300

    initial commit

--- index.php
+++ index.php
@@ -17,17 +17,33 @@
   $app->get('/', function () use($app) {
     return $app['twig']->render('home.twig', array(
       'content' => 'Добро пожаловать!',
-      'content' => 'Hello'
     ));  
   });
   
  $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма: </h2><h3>' . $n1 . '+' . $n2 .'=' .($n1 + $n2) . '</h3>'; 
+    return '<h2 style = "font-family: Arial" >Сумма: </h2><h3>' . $n1 . ' + ' . $n2 .' = ' .($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
 
-  $app->get('/mpy/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Произведение: </h2><h3>' . ($n1 * $n2) . '</h3>'; 
+ $app->get('/mpy/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Произведение: </h2><h3>' . $n1 . ' * ' . $n2 .' = ' .($n1 * $n2) . '</h3>'; 
   })->after($hdrs);
+
+ $app->get('/sub/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Вычитание: </h2><h3>' . $n1 . ' / ' . $n2 .' = ' .($n1 / $n2) . '</h3>'; 
+  })->after($hdrs);
+
+ $app->get('/div/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Деление: </h2><h3>' . $n1 . ' - ' . $n2 .' = ' .($n1 - $n2) . '</h3>'; 
+  })->after($hdrs);
+
+ $app->get('/pow/{n1}/{n2}', function ($n1, $n2) use($app) {
+    return '<h2>Возведение в степень: </h2><h3>' . $n1 . ' ^ ' . $n2 .' = ' .($n1 ** $n2) . '</h3>'; 
+  })->after($hdrs);
+
+ $app->get('/author', function () use($app) {
+    return '<h4 id="author" title="GossJS">Skorobogatov Kirill</h4>'; 
+  })->after($hdrs);
+
 
   $app->error(function ($e) use($app) {
     if ($e instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
